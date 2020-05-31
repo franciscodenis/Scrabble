21160e2054d7db08305cdd3092d8c73c93295c96
franciscodenis <francisco.denis87@gmail.com>
2020-05-31 11:23:24 -0300

    Se arregló la ventana de puntaje para mostar más de 10 puntos, se depuro la función cambiar letras

--- ProgramaPrincipal.py
+++ ProgramaPrincipal.py
@@ -31,7 +31,8 @@ def main():
     layout= tablero.crear_tablero('principiante')
     layout.append([sg.Text('Seleccione una letra de abajo', auto_size_text=True, font='Helvetica', background_color=('#5CA2A3'))])
     layout.append([sg.Button(key=(-1, i) , button_text= atril.get_espacio_fichas()[i].get_letra(), **letter_atril) for i in range(letras_de_atril)])
-    botones = [sg.Button(key='vali', button_text='Validar', button_color=('white', '#C54F1F'), font='Helvetica'),sg.Button(button_text='Cambiar letras',key ="cambiar_letras", button_color=('white', '#C54F1F'), font='Helvetica'), sg.Text('Puntaje total: ', font='Helvetica', background_color=('#5CA2A3')), sg.Text('0', key='punt', font='Helvetica', background_color=('#5CA2A3'))]
+    botones = [sg.Button(key='vali', button_text='Validar', button_color=('white', '#C54F1F'), font='Helvetica'),sg.Button(button_text='Cambiar letras',key ="cambiar_letras", button_color=('white', '#C54F1F'), font='Helvetica'), sg.Text('Puntaje total: ', font='Helvetica', background_color=('#5CA2A3')), sg.Text('000', key='punt', font='Helvetica', background_color=('#5CA2A3'))]
+    layout.append([sg.Checkbox("", key=('Checkbox', i), size=(2, 2))for i in range(letras_de_atril)])
     layout.append(botones)
 
     window = sg.Window('Scrabble', background_color='#5CA2A3').Layout(layout)
@@ -43,17 +44,13 @@ def main():
             tablero.click(atril, event, window)
 
         if event == "cambiar_letras":
-            if atril.get_cambios_atril() > 0 and len(fichas_jugador)>7:
-                atril.cambiar_letras(fichas_jugador,window,tablero)
-            else:
-                if(atril.get_cambios_atril()<1):
-                    sg.Popup('no tienes mas cambios')
+            atril.cambiar_letras(fichas_jugador,window,tablero,values,fichas_jugador)
 
         if event in atril.listado_botones():
             atril.click(tablero, event)
 
         if event == 'vali':
-            tablero.click_validar(atril, tablero, window, diccionario, puntaje_total, fichas_jugador, palabras_permitidas)
+            puntaje_total = tablero.click_validar(atril, tablero, window, diccionario, puntaje_total, fichas_jugador, palabras_permitidas)
 
         if event in (None, 'Exit'):
             break
